b71f227e250711b00f71968ec4e4c146237b4cfd
Sharmaryan <panditaryan.as@gmail.com>
2026-03-28 11:35:46 +0530

    combination sum

---
+++ sessions/2026-03-05-session39/solutions/js/combination_sum_aryan.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/combination-sum/
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    let result = []
+
+    function backtrack(remainingSum, path, start) {
+        if (remainingSum === 0) result.push([...path])
+        if (remainingSum <= 0) return
+
+        for (let i = start; i < candidates.length; i++) {
+            path.push(candidates[i])
+            backtrack(remainingSum - candidates[i], path, i)
+            path.pop()
+        }
+    }
+    backtrack(target, [], 0)
+
+    return result;
+};
