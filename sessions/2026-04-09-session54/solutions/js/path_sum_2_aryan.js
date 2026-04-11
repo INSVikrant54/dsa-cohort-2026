4ca98822cfa6ed4a774e8105b974b76d2705c655
Sharmaryan <panditaryan.as@gmail.com>
2026-04-11 11:34:39 +0530

    path sum 2

---
+++ sessions/2026-04-09-session54/solutions/js/path_sum_2_aryan.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function (root, targetSum) {
+    let result = []
+
+    function traverse(curr, sum, arr) {
+        if (!curr) return
+        arr.push(curr.val)
+        sum += curr.val
+
+        const isLeaf = !curr.left && !curr.right
+        if (isLeaf) {
+            if (sum === targetSum) result.push([...arr])
+            return
+        }
+
+        traverse(curr.left, sum, [...arr])
+        traverse(curr.right, sum, [...arr])
+    }
+
+    traverse(root, 0, [])
+    return result
+};
