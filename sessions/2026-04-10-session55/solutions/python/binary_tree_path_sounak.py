af88792b867db5b087dcd11abb3d9874a9e2eeac
Sounak <sounakume@gmail.com>
2026-04-13 23:31:34 +0530

    add binary path sol python

---
+++ sessions/2026-04-10-session55/solutions/python/binary_tree_path_sounak.py
@@ -0,0 +1,22 @@
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+
+        res = []
+        if root == None:
+            return res
+
+        val = root.val
+        
+        l1 = self.binaryTreePaths(root.left)
+        l2 = self.binaryTreePaths(root.right)
+
+        l = l1 + l2
+
+        if len(l) == 0:
+            return [f"{val}"]
+            
+        for i in range(len(l)):
+            p = f"{val}->{l[i]}"
+            res.append(p)
+        
+        return res
